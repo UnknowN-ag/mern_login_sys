fb8328ea86a9198fc5a25a600452a56ee5f74ac4
UnknowN-ag <ag528927@gmail.com>
2020-04-17 20:56:29 +0530

    component in process

---
+++ frontend/src/components/Profile.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+class Profile extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            name: '',
+            email: ''
+        }
+    }
+
+    componentDidMount() {
+        const token = localStorage.usertoken;
+        const decoded = jwt_decode(token);
+        this.setState({
+            name: decoded.name,
+            email: decoded.email
+        })
+    }
+
+    render(){
+        return (
+            <div className="container">
+                <div className="jumbotron">
+                    <div className="col-sm-8 mx-auto">
+                         <h1 className="text-center">Profile</h1>
+                    </div>
+                    <table className="table col-md-6 mx-auto">
+                        <tbody>
+                            <tr>
+                                <td>Name</td>
+                                <td>{this.state.name}</td>
+                            </tr>
+                            <tr>
+                                <td>Email</td>
+                                <td>{this.state.email}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default Profile;
